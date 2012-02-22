f34dcfcd6565010f9f28cf1b46945c3a173dccce
Mike Smith <mike@sticknet.net>
2012-02-22 01:18:58 -0600

    Ignore extraneous spaces in command definitions

--- lib/prompt/dsl_helper.rb
+++ lib/prompt/dsl_helper.rb
@@ -1,8 +1,9 @@
 module Prompt
   module DSLHelper
 
+    # Split command into an array of Strings and Parameters
     def self.words command_name, parameters
-      command_name.split(/\s/).map do |word|
+      command_name.strip.split(/\s+/).map do |word|
          if word[0] == ":"
            sym = word[1..-1].to_sym
            parameters.find {|v| v.name == sym} || Parameter.new(sym, sym.to_s)
