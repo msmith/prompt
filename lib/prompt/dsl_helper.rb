5a0ae3d4e1259f07f87fd6b461135bda366b12a7
Mike Smith <mike@sticknet.net>
2012-02-22 02:48:29 -0600

    Allow a description to be provided for glob variables

--- lib/prompt/dsl_helper.rb
+++ lib/prompt/dsl_helper.rb
@@ -6,10 +6,15 @@ module Prompt
       command_name.strip.split(/\s+/).map do |word|
          if word[0] == ":"
            sym = word[1..-1].to_sym
-           parameters.find {|v| v.name == sym} || Parameter.new(sym, sym.to_s)
+           parameters.find {|p| p.name == sym} || Parameter.new(sym)
          elsif word[0] == "*"
            sym = word[1..-1].to_sym
-           parameters.find {|v| v.name == sym} || GlobParameter.new(sym, sym.to_s)
+           param = parameters.find {|p| p.name == sym}
+           # Since GlobParameters can't be defined ahead of time in the DSL, we
+           # always create a new one and copy the description
+           # This is kinda jankey
+           desc = param ? param.desc : nil
+           GlobParameter.new(sym, desc)
          else
            word
          end
